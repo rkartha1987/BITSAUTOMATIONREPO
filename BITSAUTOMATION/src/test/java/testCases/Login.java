0bac3fef0cd7c22f1830295a9d1950698fdf719e
rkartha1987 <rkartha1987@gmail.com>
2021-12-02 14:37:42 +0530

    Update Login.java

--- BITSAUTOMATION/src/test/java/testCases/Login.java
+++ BITSAUTOMATION/src/test/java/testCases/Login.java
@@ -17,28 +17,33 @@ public class Login extends CommonFunctions{
 	static Logger logger = Logger.getLogger(Login.class);
 	
 	@Test(priority = 0)
-	public void ValidUserandinvalidPass() throws Throwable
+	public void ValidUserandInvalidPass() throws Throwable
 	{
+		PageFactory.initElements(driver, Login_Page_Objects.class);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		extenttestCase=extentReport.createTest("Login Authentication Validation");
+		extenttestCase=extentReport.createTest("Login Authentication Validation : ValidUserandinvalidPass");
 		logger.info("Launching the Application");
 		logger.info("Checking Login with valid UserId and invalid Password");
-		PageFactory.initElements(driver, Login_Page_Objects.class);
+				
+		/*extenttestCase.log(Status.INFO, "Verifying Login Authentication with Valid User Id and Invalid Password");
+		Login_Page_Objects.UserId.sendKeys(getExcelData("Login", 9, 0));
+		Thread.sleep(2000);
+		Login_Page_Objects.Password.sendKeys(getExcelData("Login", 9, 1));
+		Thread.sleep(2000); 
+		Login_Page_Objects.SignIn.click();
+		CommonFunctions.captureScreen("LoginPage - Valid User and Invalid Pass.png");
+		String LoginWarn = Login_Page_Objects.LoginWarn.getText();
 		
-		/*
-		 * extenttestCase.log(Status.INFO,
-		 * "Verifying Login Authentication with Valid User Id and Invalid Password");
-		 * 
-		 * Login_Page_Objects.UserId.sendKeys(getExcelData("Login", 1, 0));
-		 * Thread.sleep(2000);
-		 * 
-		 * Login_Page_Objects.Password.sendKeys(getExcelData("Login", 1, 1));
-		 * Thread.sleep(2000);
-		 * 
-		 * Login_Page_Objects.SignIn.click();
-		 */
+		if(LoginWarn.contentEquals("Invalid User Id or password"))
+		{
+			extenttestCase.log(Status.PASS, "Login validation done with Invalid User Id and Password");
+		}		
+		else
+		{
+			extenttestCase.log(Status.INFO, "Login validation Failed with Invalid User Id and Password");
+		}*/
 	}
-	
+
 	@Test(priority = 1)
 	public void InvalidUserIdandPassword() throws Throwable
 	{
@@ -50,62 +55,23 @@ public class Login extends CommonFunctions{
 		Login_Page_Objects.UserId.sendKeys(getExcelData("Login", 8, 0));
 		Thread.sleep(2000);
 		Login_Page_Objects.Password.clear();
-		Thread.sleep(2000);
-		
+		Thread.sleep(2000);		
 		Login_Page_Objects.Password.sendKeys(getExcelData("Login", 8, 1));
-		Thread.sleep(2000);
-		
+		Thread.sleep(2000);		
 		Login_Page_Objects.SignIn.click();
 		Thread.sleep(2000);
-		
+		CommonFunctions.captureScreen("LoginPage.png");
 		String LoginWarn = Login_Page_Objects.LoginWarn.getText();
 		
 		if(LoginWarn.contentEquals("Invalid User Id or password"))
 		{
 			extenttestCase.log(Status.PASS, "Login validation done with Invalid User Id and Password");
-		}
-		
+		}		
 		else
 		{
 			extenttestCase.log(Status.INFO, "Login validation Failed with Invalid User Id and Password");
-		}
-		
-	}
-	
-	@Test(enabled = false)
-	public void InvalidUserandPassword() throws Throwable
-	{
-		extenttestCase.log(Status.INFO, "Verifying Login Authentication with Invalid User Id and Invalid Password");
-		
-		Login_Page_Objects.UserId.clear();
-		Thread.sleep(2000);
-		
-		Login_Page_Objects.UserId.sendKeys(getExcelData("Login", 8, 0));
-		Thread.sleep(2000);
-		
-		Login_Page_Objects.Password.clear();
-		Thread.sleep(2000);
-		
-		Login_Page_Objects.Password.sendKeys(getExcelData("Login", 8, 1));
-		Thread.sleep(2000);
-		
-		Login_Page_Objects.SignIn.click();
-		Thread.sleep(3500);
-		
-		String LoginWarn = Login_Page_Objects.LoginWarn.getText();
-		System.out.println(LoginWarn);
-		
-		if(LoginWarn.contentEquals("Invalid User Id or password"))
-		{
-			extenttestCase.log(Status.PASS, "Login validation done with Invalid User Id and Invalid Password");
-		}
-		
-		else
-		{
-			extenttestCase.log(Status.INFO, "Login validation Failed with Invalid User Id and Invalid Password");
-		}
-		
-	}
+		}		
+	}	
 	
 	@Test(priority = 2)
 	public void ValidUserIdandPassword() throws Throwable
@@ -130,10 +96,12 @@ public class Login extends CommonFunctions{
 		if (Login_Page_Objects.WelcomeMsg.getText().toString().contains("Welcome"))
 		{
 			extenttestCase.log(Status.PASS, "Login validation done with Valid User Id and Password");
+			CommonFunctions.captureScreen("UserLogin - Dashboard.png");
 		}
 		else
 		{
 			extenttestCase.log(Status.FAIL, "Login validation not successfull");
+			CommonFunctions.captureScreen("UserLoginFail.png");
 		}	
 	}
 }
